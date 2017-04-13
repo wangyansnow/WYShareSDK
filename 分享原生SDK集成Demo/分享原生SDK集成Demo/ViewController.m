f4313c7cf9e3943e1898375ffba2c797428220ad
wangyansnow <13146597377@163.com>
2017-04-13 14:53:10 +0800

    修改ViewController中的各种分享使用WYShareSDK进行分享

--- 分享原生SDK集成Demo/分享原生SDK集成Demo/ViewController.m
+++ 分享原生SDK集成Demo/分享原生SDK集成Demo/ViewController.m
@@ -16,6 +16,7 @@
 #import "AppDelegate.h"
 #import "WYParamObj.h"
 
+
 @interface ViewController ()
 
 @end
@@ -45,46 +46,40 @@
     _wbScene = (int)sender.selectedSegmentIndex;
 }
 
+#define WYShareFinished(response) \
+    if (response.isSucess) { \
+        NSLog(@"分享成功"); \
+        return; \
+    } \
+    NSLog(@"分享失败 error = %@", response.errorStr);
+
 #pragma mark - ShareMethods
 #pragma mark - 手机QQ分享 [只有`新闻`(网页)和音乐可以分享到朋友圈]
 - (IBAction)qqTextShare {
-    HasQQInstall
-    
-    QQApiTextObject *textObj = [QQApiTextObject objectWithText:@"qq分享"];
-    SendMessageToQQReq *textReq = [SendMessageToQQReq reqWithContent:textObj];
-    [QQApiInterface sendReq:textReq];
+    [WYShareSDK wy_qqShareText:@"qq分享" finshed:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)qqImageShare {
-    HasQQInstall
-    
     //开发者分享图片数据
     NSData *imgData = UIImagePNGRepresentation([UIImage imageNamed:@"LaunchImage_iOS_40_confident"]);
-    QQApiImageObject *imgObj = [QQApiImageObject objectWithData:imgData
-                                               previewImageData:imgData
-                                                          title:@"title"
-                                                   description :@"description"];
-    SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:imgObj];
-    //将内容分享到qq
-    [QQApiInterface sendReq:req];
+    [WYShareSDK wy_qqShareImage:imgData originalImage:imgData title:@"title" description:@"description" finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)qqWebShare {
-    HasQQInstall
-    
     NSString *url = @"https://www.wanglibao.com/";
     UIImage *previewImage = [UIImage imageNamed:@"ic_account_tyj"];
     NSData *previewData = UIImagePNGRepresentation(previewImage);
     
-    QQApiNewsObject *newsObject = [QQApiNewsObject objectWithURL:[NSURL URLWithString:url] title:@"qq news share" description:@"news short description" previewImageData:previewData targetContentType:QQApiURLTargetTypeNews];
-    
-    SendMessageToQQReq *newsReq = [SendMessageToQQReq reqWithContent:newsObject];
-    [self qqSendRequest:newsReq];
+    [WYShareSDK wy_qqShareWebURL:url description:@"news short description" thumbImage:previewImage title:@"qq news share" scene:_qqScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)qqMusicShare {
-    HasQQInstall
-    
     // 1.分享跳转URL
     NSString *url = @"https://www.wanglibao.com/";
     // 2.分享预览图URL地址 / 也可以是NSData
@@ -92,226 +87,77 @@
     // 3.音乐播放的网络流媒体地址
     NSString *flashURL = kMusicURL;
     
-    QQApiAudioObject *audioObject = [QQApiAudioObject objectWithURL:[NSURL URLWithString:url] title:@"title" description:@"description" previewImageURL:[NSURL URLWithString:previewImageUrl]];
-    // 4.设置播放流媒体地址
-    [audioObject setFlashURL:[NSURL URLWithString:flashURL]];
-    
-    SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:audioObject];
-    [self qqSendRequest:req];
-}
-
-- (void)qqSendRequest:(QQBaseReq *)req {
-    if (_qqScene == 0) { // 会话
-        [QQApiInterface sendReq:req];
-        return;
-    }
-    // 朋友圈
-    [QQApiInterface SendReqToQZone:req];
+    [WYShareSDK wy_qqShareMusicURL:flashURL jumpURL:url previewImageURL:previewImageUrl previewImageData:nil title:@"title" description:@"description" scene:_qqScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 #pragma mark - 微信分享 [文字不可以分享到朋友圈]
 - (IBAction)weChatTextShare {
-    HasWXInstall
-    
-    SendMessageToWXReq *textReq = [[SendMessageToWXReq alloc] init];
-    
-    textReq.bText = YES;
-    textReq.text = @"王俨 send to wechat";
-    textReq.scene = WXSceneSession;
-    
-    [WXApi sendReq:textReq];
+    [WYShareSDK wy_weChatShareText:@"王俨 send to wechat" finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)weChatImageShare {
-    HasWXInstall
-    
-    WXMediaMessage *message = [WXMediaMessage message];
-    [message setThumbImage:[UIImage imageNamed:@"ic_account_tyj"]];
-    
-    WXImageObject *imageObject = [WXImageObject object];
     NSString *path = [[NSBundle mainBundle] pathForResource:@"LaunchImage_iOS_40_confident.png" ofType:nil];
-    imageObject.imageData = [NSData dataWithContentsOfFile:path];
     
-    message.mediaObject = imageObject;
-    
-    SendMessageToWXReq *imageReq = [[SendMessageToWXReq alloc] init];
-    imageReq.bText = NO;
-    imageReq.message = message;
-    imageReq.scene = _wxScene;
-    [WXApi sendReq:imageReq];
+    [WYShareSDK wy_weChatShareThumbImage:[UIImage imageNamed:@"ic_account_tyj"] originalImage:[NSData dataWithContentsOfFile:path] scene:_wxScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)weChatWebShare {
-    HasWXInstall
-    
-    WXMediaMessage *message = [WXMediaMessage message];
-    message.title = @"标题";
-    message.description = @"描述";
-    [message setThumbImage:[UIImage imageNamed:@"ic_account_tyj"]];
-    
-    WXWebpageObject *webpageObject = [WXWebpageObject object];
-    webpageObject.webpageUrl = @"https://www.wanglibao.com/";
-    message.mediaObject = webpageObject;
-    
-    SendMessageToWXReq *webReq = [[SendMessageToWXReq alloc] init];
-    webReq.bText = NO;
-    webReq.message = message;
-    webReq.scene = _wxScene;
-    [WXApi sendReq:webReq];
+    [WYShareSDK wy_weChatShareWebURL:@"https://www.wanglibao.com/" description:@"description" thumbImage:[UIImage imageNamed:@"ic_account_tyj"] title:@"title" scene:_wxScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)weChatMusicShare {
-    HasWXInstall
-    
-    WXMediaMessage *message = [WXMediaMessage message];
-    message.title = @"音乐title";
-    message.description = @"音乐description";
-    [message setThumbImage:[UIImage imageNamed:@"ic_account_tyj"]];
-    
-    WXMusicObject *musicObj = [WXMusicObject object];
-    musicObj.musicUrl = kWechatMusicURL;  // 音乐网页url
-    musicObj.musicDataUrl = @"http://stream20.qqmusic.qq.com/32464723.mp3";  // 音乐数据url
-    message.mediaObject = musicObj;
-    
-    SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
-    req.bText = NO;
-    req.message = message;
-    req.scene = _wxScene;
-    
-    [WXApi sendReq:req];
+    [WYShareSDK wy_weChatShareMusicURL:kWechatMusicURL musicDataURL:@"http://stream20.qqmusic.qq.com/32464723.mp3" thumbImage:[UIImage imageNamed:@"ic_account_tyj"] title:@"音乐title" description:@"音乐description" scene:_wxScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)weChatVideoShare {
-    HasWXInstall
-    
-    WXMediaMessage *message = [WXMediaMessage message];
-    message.title = @"视频 title";
-    message.description = @"视频 description";
-    [message setThumbImage:[UIImage imageNamed:@"ic_account_tyj"]];
-    
-    WXVideoObject *videoObj = [WXVideoObject object];
-    videoObj.videoUrl = @"http://v.youku.com/v_show/id_XNTUxNDY1NDY4.html";
-    message.mediaObject = videoObj;
-    
-    SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
-    req.bText = NO;
-    req.message = message;
-    req.scene = _wxScene;
-    
-    [WXApi sendReq:req];
+    [WYShareSDK wy_weChatShareVideoURL:@"http://v.youku.com/v_show/id_XNTUxNDY1NDY4.html" thumbImage:[UIImage imageNamed:@"ic_account_tyj"] title:@"视频 title" description:@"视频 description" scene:_wxScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 #pragma mark - 微博分享
 - (IBAction)weiboTextShare {
-    HasWeiboInstall
-    
-    WBMessageObject *message = [WBMessageObject message];
-    message.text = @"王的俨的分享的的的";
-    
-    [WeiboSDK sendRequest:[self weiboRequestWithMessage:message]];
+    [WYShareSDK wy_weiboShareText:@"王的俨的分享的的的" scene:_wbScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)weiboImageShare {
-    HasWeiboInstall
-    
-    WBMessageObject *message = [WBMessageObject message];
-    
-    WBImageObject *imageObj = [WBImageObject object];
     UIImage *previewImage = [UIImage imageNamed:@"ic_account_tyj"];
-    imageObj.imageData = UIImagePNGRepresentation(previewImage);
-    
-    message.imageObject = imageObj;
-    
-    [WeiboSDK sendRequest:[self weiboRequestWithMessage:message]];
+    [WYShareSDK wy_weiboShareImage:UIImagePNGRepresentation(previewImage) scene:_wbScene finshed:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 - (IBAction)weiboWebShare {
-    HasWeiboInstall
-    
-    WBMessageObject *message = [WBMessageObject message];
-    WBWebpageObject *webpage = [WBWebpageObject object];
-    webpage.objectID = @"";
-    webpage.title = @"网利宝--全民淘金";
-    webpage.description = @"微博分享内容--网利宝";
     UIImage *previewImage = [UIImage imageNamed:@"ic_account_tyj"];
-    webpage.thumbnailData = UIImagePNGRepresentation(previewImage);
-    webpage.webpageUrl = @"https://www.wanglibao.com";
-    message.mediaObject = webpage;
-    
-    [WeiboSDK sendRequest:[self weiboRequestWithMessage:message]];
+    [WYShareSDK wy_weiboShareWebURL:@"https://www.wanglibao.com" title:@"title" description:@"description" thumbImage:UIImagePNGRepresentation(previewImage) scene:_wbScene finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 /// 只支持朋友圈内分享
 - (IBAction)weiboMusicShare {
-    HasWeiboInstall
-    
-    WBMessageObject *message = [WBMessageObject message];
-    
-    WBMusicObject *musicObject = [WBMusicObject object];
-    musicObject.objectID = @"";
-    musicObject.title = @"微博音乐分享";
-    musicObject.description = @"王俨分享的音乐到微博的详细描述description";
-    musicObject.thumbnailData = UIImagePNGRepresentation([UIImage imageNamed:@"ic_account_tyj"]);
-    musicObject.musicUrl = @"http://stream20.qqmusic.qq.com/32464723.mp3";
-    musicObject.musicStreamUrl = @"http://stream20.qqmusic.qq.com/32464723.mp3";
-    
-    message.mediaObject = musicObject;
-    [WeiboSDK sendRequest:[WBSendMessageToWeiboRequest requestWithMessage:message]];
+    [WYShareSDK wy_weiboShareMusicURL:@"http://stream20.qqmusic.qq.com/32464723.mp3" streamURL:@"http://stream20.qqmusic.qq.com/32464723.mp3" title:@"微博音乐分享" description:@"王俨分享的音乐到微博的详细描述description" thumbnailData:UIImagePNGRepresentation([UIImage imageNamed:@"ic_account_tyj"]) finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
+    }];
 }
 
 /// 只支持朋友圈内分享
 - (IBAction)weiboVideoShare {
-    HasWeiboInstall
-    
-    WBMessageObject *message = [WBMessageObject message];
-    
-    WBVideoObject *videoObject = [WBVideoObject object];
-    videoObject.objectID = @"";
-    videoObject.title = @"视频分享";
-    videoObject.description = @"这个视频很好看";
-    videoObject.thumbnailData = UIImagePNGRepresentation([UIImage imageNamed:@"ic_account_tyj"]);
-    videoObject.videoUrl = @"http://v.youku.com/v_show/id_XNTUxNDY1NDY4.html";
-    videoObject.videoStreamUrl = videoObject.videoUrl;
-    
-    message.mediaObject = videoObject;
-    [WeiboSDK sendRequest:[WBSendMessageToWeiboRequest requestWithMessage:message]];
-}
-
-- (WBBaseRequest *)weiboRequestWithMessage:(WBMessageObject *)message {
-    if (_wbScene == 0) { // 会话
-        return [WBShareMessageToContactRequest requestWithMessage:message];
-    }
-    // 朋友圈
-    return [WBSendMessageToWeiboRequest requestWithMessage:message];
-}
-
-- (IBAction)shareSDKBtnClick {
-    [WYShareSDK wy_weChatShareText:@"分享一个" finished:^(WYShareResponse *response) {
-        if (response.isSucess) {
-            NSLog(@"分享成功");
-            return;
-        }
-        NSLog(@"分享失败 error = %@", response.errorStr);
-    }];
-}
-
-- (IBAction)qqTextShareBtnClick:(UIButton *)sender {
-    [WYShareSDK wy_qqShareText:@"qq分享" finshed:^(WYShareResponse *response) {
-        if (response.isSucess) {
-            NSLog(@"分享成功");
-            return;
-        }
-        NSLog(@"分享失败 error = %@", response.errorStr);
-    }];
-}
-
-- (IBAction)weiboTextShareBtnClick:(UIButton *)sender {
-    [WYShareSDK wy_weiboShareText:@"微博分享" scene:WeiboShareSceneTimeline finished:^(WYShareResponse *response) {
-        if (response.isSucess) {
-            NSLog(@"分享成功");
-            return;
-        }
-        NSLog(@"分享失败 error = %@", response.errorStr);
+    NSString *videoUrl = @"http://v.youku.com/v_show/id_XNTUxNDY1NDY4.html";
+    [WYShareSDK wy_weiboShareVideoURL:videoUrl streamURL:videoUrl title:@"视频分享" description:@"这个视频很好看" thumbnailData:UIImagePNGRepresentation([UIImage imageNamed:@"ic_account_tyj"]) finished:^(WYShareResponse *response) {
+        WYShareFinished(response);
     }];
 }
 
@@ -325,26 +171,7 @@
         
         NSLog(@"wxToken = %@", wxToken);
         NSLog(@"wxUserinfo = %@", wxUserinfo);
-        [self log];
     }];
-
-//    BOOL obj = [self classTest];
-//    NSLog(@"obj = %@", obj);
-//    NSLog(@"obj = %d", [self performSelector:@selector(test:) withObject:@[@"minyan"]]);
-}
-
-- (id)classTest {
-    Class cls = [self class];
-    return [self performSelector:@selector(test:) withObject:@[@"minyan"]];
-}
-
-- (void)test:(NSArray *)arr {
-    NSLog(@"arr = %@", arr);
-//    return YES;
-}
-
-- (void)log {
-    NSLog(@"hahah");
 }
 
 - (IBAction)qqLoginBtnClick {
